f5fafafd6fb134865a45e376e64f0fb2a497cd52
Florent Ulvé <florent.ulve@gmail.com>
2018-03-13 22:12:13 +0100

    HoC withAnimationOnClick now display WrapComponent name

--- src/components/Utils/withAnimationOnClick.jsx
+++ src/components/Utils/withAnimationOnClick.jsx
@@ -3,41 +3,44 @@ import ReactDOM from 'react-dom';
 
 export default function withAnimationOnClick(WrappedComponent, config) {
   
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = { isAnimated: false};
-      console.log('animation '+ config.animation);
-      this.animation = config.animation + ' jello animated ';
-    }
+    class WithAnimationOnClick extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = { isAnimated: false};
+            console.log('animation '+ config.animation);
+            this.animation = config.animation + ' jello animated ';
+        }
 
-    componentDidMount() {
-        ReactDOM.findDOMNode(this).addEventListener('animationend', this.onAnimationEnd.bind(this), false);
-    }
+        componentDidMount() {
+            ReactDOM.findDOMNode(this).addEventListener('animationend', this.onAnimationEnd.bind(this), false);
+        }
 
-    componentWillUnmount() {
-        ReactDOM.findDOMNode(this).removeEventListener('animationend', this.onAnimationEnd.bind(this), false);
-    }
+        componentWillUnmount() {
+            ReactDOM.findDOMNode(this).removeEventListener('animationend', this.onAnimationEnd.bind(this), false);
+        }
 
-    onAnimationEnd(){
-        this.setState({
-            isAnimated: false
-        })
-    }
+        onAnimationEnd(){
+            this.setState({
+                isAnimated: false
+            })
+        }
 
-    onClick() {
-        console.log('click')
-        this.setState({
-            isAnimated : true
-        });
-    }
+        onClick() {
+            console.log('click')
+            this.setState({
+                isAnimated : true
+            });
+        }
 
-    render() {
-      return (
-        <WrappedComponent className={this.state.isAnimated ? this.animation : ''} onClick={this.onClick.bind(this)} {...this.props}>
-        {this.props.children}
-        </WrappedComponent>
-      );
+        render() {
+        return (
+            <WrappedComponent className={this.state.isAnimated ? this.animation : ''} onClick={this.onClick.bind(this)} {...this.props}>
+            {this.props.children}
+            </WrappedComponent>
+        );
     }
-  };
+};
+
+    WithAnimationOnClick.displayName = `withAnimationOnClick(${WrappedComponent.displayName || WrappedComponent.name})`;
+    return WithAnimationOnClick;
 }
